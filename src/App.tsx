d236f9a731815146e21416445d94ef72efd9667d
Ozodbek <bakhriddinoff01@gmail.com>
2025-12-21 14:56:08 +0500

    add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import Header from "./components/Header.tsx";
+import Hero from "./components/Hero.tsx";
+import About from "./components/About.tsx";
+import Projects from "./components/Projects.tsx";
+import Faq from "./components/Faq.tsx";
+import Experience from "./components/Experience.tsx";
+import Approach from "./components/Approach.tsx";
+
 export default function App() {
   return (
-    <div>
-      
+    <div className="bg-[#01041A] w-full h-screen">
+      <Header/>
+      <Hero/>
+      <About/>
+      <Projects/>
+      <Faq/>
+      <Experience/>
+      <Approach/>
     </div>
   )
 }
---
+++ src/components/Approach.tsx
@@ -0,0 +1,7 @@
+export default function Approach() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
