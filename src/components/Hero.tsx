031ca39400882373c27f0ebe2da2edc75f870c4c
Ozodbek <bakhriddinoff01@gmail.com>
2025-12-21 19:57:26 +0500

    fixed code

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
             <h1 className="pb-6 xl:pb-7.5 text-[48px] xl:text[72px] font-bold leading-none text-center">Transforming Concepts into <br className="hidden lg:block"/> Seamless <span className="text-[#CBACF9]">User Experiences</span></h1>
             <p className="text-[16px] xl:text-[24px] xl:7.5 w-full text-center pb-5 xl:pb-[7.5] ">Hi! I’m <span className="hidden xl:inline">Bakhriddinov</span> Ozodbek, <br className="lg:hidden"/> a Next.js Developer based in Uzbekistan</p>
             <button 
-            className="w-[90vw] lg:w-52 h-14 flex justify-center items-center gap-2 border-2 border-[#1B1E33] rounded-md lg:rounded-[14px]"
+            className="cursor-pointer active:text-gray-300 active:w-53 active:h-15 w-[90vw] lg:w-52 h-14 flex justify-center items-center gap-2 border-2 border-[#1B1E33] rounded-md lg:rounded-[14px]"
             style={{
                 background: "linear-gradient(to right, #272A3C, #0F132A)",
             }}            >
