45bde75e050f57b50428412eb7e9cb057a572baa
Ozodbek <bakhriddinoff01@gmail.com>
2025-12-22 23:05:27 +0500

    fixed code

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import arrow from "../assets/Arrow.png"
 export default function Hero() {
     return (
         <div className="flex flex-col justify-center items-center pt-16">
-            <p className="text-[12px] text-[#E4ECFF] md:text-14px lg:text-[16px] pb-5 xl:pb-5.5 font-normal">Dynamic Web Magic with Next.js</p>
+            <p className="text-[12px] text-[#E4ECFF] md:text-14px lg:text-[16px] pb-5 xl:pb-5.5 font-normal">DYNAMIC WEB MAJIC WITH NEXT.JS</p>
             <h1 className="pb-6 xl:pb-7.5 text-[48px] xl:text[72px] font-bold leading-none text-center">Transforming Concepts into <br className="hidden lg:block"/> Seamless <span className="text-[#CBACF9]">User Experiences</span></h1>
             <p className="text-[16px] xl:text-[24px] xl:7.5 w-full text-center pb-5 xl:pb-[7.5] ">Hi! I’m <span className="hidden xl:inline">Bakhriddinov</span> Ozodbek, <br className="lg:hidden"/> a Next.js Developer based in Uzbekistan</p>
             <button 
